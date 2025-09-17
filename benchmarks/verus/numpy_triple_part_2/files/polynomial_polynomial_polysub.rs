f012552ad3a1b80b2c813a406b3335304c76f9f8
astefano <lacramioara.astefanoaei@gmail.com>
2025-09-17 11:24:33 +0200

    add 22 more

---
+++ benchmarks/verus/numpy_triple_part_2/files/polynomial_polynomial_polysub.rs
@@ -0,0 +1,37 @@
+// <vc-preamble>
+use vstd::prelude::*;
+
+verus! {
+// </vc-preamble>
+
+// <vc-helpers>
+// </vc-helpers>
+
+// <vc-spec>
+fn polysub(c1: Vec<i32>, c2: Vec<i32>) -> (result: Vec<i32>)
+    ensures
+        result.len() == if c1.len() >= c2.len() { c1.len() } else { c2.len() },
+        forall|i: int| 0 <= i < result.len() ==> {
+            if i < c1.len() && i < c2.len() {
+                result[i] == c1[i] - c2[i]
+            } else if i < c1.len() && i >= c2.len() {
+                result[i] == c1[i]
+            } else if i >= c1.len() && i < c2.len() {
+                result[i] == 0 - c2[i]
+            } else {
+                result[i] == 0
+            }
+        }
+// </vc-spec>
+// <vc-code>
+{
+    // impl-start
+    assume(false);
+    unreached()
+    // impl-end
+}
+// </vc-code>
+
+
+}
+fn main() {}
