59027a0dae1b2327729bc5fe9c57574f1aa8ff20
astefano <lacramioara.astefanoaei@gmail.com>
2025-09-21 09:29:32 +0200

    fix 3 verus from he

--- benchmarks/verus/humaneval/files/humaneval_135.rs
+++ benchmarks/verus/humaneval/files/humaneval_135.rs
@@ -13,11 +13,15 @@ spec fn has_decrease_at(arr: Seq<int>, i: int) -> bool {
 
 spec fn is_largest_decrease_index(arr: Seq<int>, result: int) -> bool {
     has_decrease_at(arr, result) && 
-    (forall|j: int| #[trigger] arr.index(j) >= 0 && result < j < arr.len() ==> arr.index(j) >= arr.index(j-1))
+    (forall|j: int| result < j < arr.len() ==> #[trigger] arr[j] >= arr[j-1])
 }
 
 spec fn is_non_decreasing(arr: Seq<int>) -> bool {
-    forall|i: int| #[trigger] arr.index(i) >= 0 && 1 <= i < arr.len() ==> arr.index(i) >= arr.index(i-1)
+    forall|i: int| 1 <= i < arr.len() ==> #[trigger] arr[i] >= arr[i-1]
+}
+
+spec fn seq_map_to_int(arr: Seq<i8>) -> Seq<int> {
+    arr.map(|_i: int, x: i8| x as int)
 }
 // </vc-preamble>
 
@@ -27,12 +31,12 @@ spec fn is_non_decreasing(arr: Seq<int>) -> bool {
 // <vc-spec>
 fn can_arrange(arr: Vec<i8>) -> (result: i8)
   requires 
-      valid_input(arr@),
+      valid_input(seq_map_to_int(arr@)),
   ensures 
-      result == -1 || (0 < result as int && result as int < arr.len()),
-      result == -1 ==> is_non_decreasing(arr@),
-      result != -1 ==> is_largest_decrease_index(arr@, result as int),
-      result != -1 ==> (exists|i: int| has_decrease_at(arr@, i)),
+      (result == -1) || (0 < result as int && (result as int) < (arr@.len() as int)),
+      result == -1 ==> is_non_decreasing(seq_map_to_int(arr@)),
+      result != -1 ==> is_largest_decrease_index(seq_map_to_int(arr@), result as int),
+      result != -1 ==> (exists|i: int| has_decrease_at(seq_map_to_int(arr@), i))
 // </vc-spec>
 // <vc-code>
 {
