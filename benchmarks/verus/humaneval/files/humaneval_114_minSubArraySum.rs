44e45a86293fb4bd219bc6cbe0853afe175672d1
Shaowei Lin <shaowei@gmail.com>
2025-09-20 22:07:07 -0700

    Fix verus humaneval_114

--- benchmarks/verus/humaneval/files/humaneval_114_minSubArraySum.rs
+++ benchmarks/verus/humaneval/files/humaneval_114_minSubArraySum.rs
@@ -22,9 +22,9 @@ spec fn sum(a: Seq<int>, s: int, t: int) -> int
 // </vc-helpers>
 
 // <vc-spec>
-fn min_sub_array_sum(a: Seq<int>) -> (s: int)
+fn min_sub_array_sum(a: Vec<i8>) -> (s: i8)
   ensures forall|p: int, q: int| 0 <= p <= q <= a@.len() ==> sum(a@.map(|i, x| x as int), p, q) >= s as int &&
-        (exists|k: int, m: int| #[trigger] sum(a@.map(|i, x| x as int), k, m) && 0 <= k <= m <= a@.len() && s as int == sum(a@.map(|i, x| x as int), k, m))
+          (exists|k: int, m: int| #[trigger] sum(a@.map(|i, x| x as int), k, m) && 0 <= k <= m <= a@.len() && s as int == sum(a@.map(|i, x| x as int), k, m))
 // </vc-spec>
 // <vc-code>
 {
