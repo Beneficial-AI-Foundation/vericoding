690ebd6c4fd441ff5d04b5e7934584c1df2400e1
Shaowei Lin <shaowei@gmail.com>
2025-09-20 22:00:12 -0700

    Move non_compiling files to yaml

---
+++ benchmarks/verus/humaneval/files/humaneval_135.rs
@@ -0,0 +1,49 @@
+// <vc-preamble>
+use vstd::prelude::*;
+
+verus! {
+
+spec fn valid_input(arr: Seq<int>) -> bool {
+    forall|i: int, j: int| 0 <= i < j < arr.len() ==> arr[i] != arr[j]
+}
+
+spec fn has_decrease_at(arr: Seq<int>, i: int) -> bool {
+    1 <= i < arr.len() && arr[i] < arr[i-1]
+}
+
+spec fn is_largest_decrease_index(arr: Seq<int>, result: int) -> bool {
+    has_decrease_at(arr, result) && 
+    (forall|j: int| #[trigger] arr.index(j) >= 0 && result < j < arr.len() ==> arr.index(j) >= arr.index(j-1))
+}
+
+spec fn is_non_decreasing(arr: Seq<int>) -> bool {
+    forall|i: int| #[trigger] arr.index(i) >= 0 && 1 <= i < arr.len() ==> arr.index(i) >= arr.index(i-1)
+}
+// </vc-preamble>
+
+// <vc-helpers>
+// </vc-helpers>
+
+// <vc-spec>
+fn can_arrange(arr: Vec<i8>) -> (result: i8)
+  requires 
+      valid_input(arr@),
+  ensures 
+      result == -1 || (0 < result as int && result as int < arr.len()),
+      result == -1 ==> is_non_decreasing(arr@),
+      result != -1 ==> is_largest_decrease_index(arr@, result as int),
+      result != -1 ==> (exists|i: int| has_decrease_at(arr@, i)),
+// </vc-spec>
+// <vc-code>
+{
+    // impl-start
+    assume(false);
+    unreached()
+    // impl-end
+}
+// </vc-code>
+
+
+}
+
+fn main() {}
